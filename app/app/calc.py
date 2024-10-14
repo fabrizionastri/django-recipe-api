1fb0a6090de4d99b7724cb1ee4ed30e5d561c31b
Fabrizio <fabrizio.nastri@flexup.org>
2024-10-14 21:25:00 +0200

    implemented basic tests

---
+++ app/app/calc.py
@@ -0,0 +1,10 @@
+# app/app/calc.py
+""" Calculator app with basic functions """
+
+def add(x, y):
+    """ Add two numbers """
+    return x + y
+
+def substract(x, y):
+    """ Substract two numbers """
+    return y - x
---
+++ app/app/tests.py
@@ -0,0 +1,17 @@
+# app/app/tests.py
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+from app.calc import add, substract
+
+class CalcTests(SimpleTestCase):
+    """ Sample tests for the calculator app """
+    def test_add(self):
+        """ Test that two numbers are added together """
+        self.assertEqual(add(3, 8), 11)
+
+    def test_substract(self):
+        """ Test that values are subtracted and returned """
+        self.assertEqual(substract(10, 15), 5)
