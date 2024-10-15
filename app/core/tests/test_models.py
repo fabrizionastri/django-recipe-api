620a290180abd1f132b70826881f6b337957092b
Fabrizio <fabrizio.nastri@flexup.org>
2024-10-15 18:35:31 +0200

    tested email validation tests

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -20,3 +20,22 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """ Test the email for a new user is normalized """
+        print('test_new_user_email_normalized')
+        sample_emails = [
+            [ 'test1@EXAMPLE.com', 'test1@example.com' ],
+            [ 'Test2@EXAMPLE.com', 'Test2@example.com' ],           # As per email standard, the domain part of the email is case-insensitive, but the local part is case-sensitive. However, most email providers treat the local part as case-insensitive as well. Since we don't know which email provider the user is using, we should treat the local part as case-sensitive, and not convert it to lowercase.
+            [ 'TEST3@EXAMPLE.com', 'TEST3@example.com' ],
+            [ 'test4@example.com', 'test4@example.com'  ],
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'pwd123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """ Test creating user without an email raises error """
+        print('test_new_user_without_email_raises_error')
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'pwd123')
